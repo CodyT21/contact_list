71608883bcfc0b7314395ad5e90d50ebf5a4a645
Cody Thomas <cody.t.thomas@gmail.com>
2022-12-30 14:29:27 -0700

    Added basic form to create and display a new contact and their contact info

--- contact_list.rb
+++ contact_list.rb
@@ -10,11 +10,37 @@ configure do
 end
 
 before do
-  session[:contacts] ||= {}
+  session[:contacts] ||= []
 end
 
 # render index page
 get '/' do
+  redirect '/contacts'
+end
+
+get '/contacts' do
   @contacts = session[:contacts]
   erb :index
+end
+
+# render new contact page
+get '/contacts/new' do
+  erb :new_contact
+end
+
+# create new contact
+post '/contacts' do
+  fname = params[:first_name]
+  lname = params[:last_name]
+  phone = params[:phone_number]
+  email = params[:email_address]
+
+  session[:contacts] << {
+                          first_name: fname,
+                          last_name: lname,
+                          phone_number: phone,
+                          email_address: email
+                        }
+  session[:message] = 'The contact has been created successfully.'
+  redirect '/contacts'
 end
