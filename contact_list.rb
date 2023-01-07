20a2217dba7ac6ace551d2c21eb55cf4423a3330
Cody Thomas <cody.t.thomas@gmail.com>
2023-01-07 09:29:29 -0700

    Included pg gem and began changing logic to use methods that will interact with the database

--- contact_list.rb
+++ contact_list.rb
@@ -1,6 +1,9 @@
 require 'sinatra'
 require 'sinatra/reloader'
 require 'tilt/erubis'
+require 'pg'
+
+require_relative 'database_persistance'
 
 configure do
   enable :sessions
@@ -9,14 +12,14 @@ configure do
   set :erb, :escape_html => true
 end
 
-def valid_contact?(contact)
-  if !valid_name?(contact[:first_name])
+def valid_contact?(contact_info*)
+  if !valid_name?(first_name)
     'First Name must only contain between 1 and 100 alphanumeric characters.'
-  elsif !valid_name?(contact[:last_name])
+  elsif !valid_name?(last_name)
     'Last Name must only contain between 1 and 100 alphanumeric characters.'
-  elsif !valid_phone_number?(contact[:phone_number])
+  elsif !valid_phone_number?(phone_number)
     'Phone Number must only contains between 10 and 11 digits.'
-  elsif !valid_email?(contact[:email_address])
+  elsif !valid_email?(email_address)
     'Email Address must be a valid email address.'
   end
 end
@@ -38,7 +41,8 @@ def sort_contacts_by_group(contacts, &block)
 end
 
 before do
-  session[:contacts] ||= []
+  # session[:contacts] ||= []
+  @storage = DatabasePeristance.new(logger)
 end
 
 # render index page
@@ -47,7 +51,8 @@ get '/' do
 end
 
 get '/contacts' do
-  @contacts = session[:contacts]
+  # @contacts = session[:contacts]
+  @contacts = @storage.all_contacts
   erb :index
 end
 
@@ -58,22 +63,23 @@ end
 
 # create new contact
 post '/contacts' do
-  fname = params[:first_name].strip
-  lname = params[:last_name].strip
-  phone = params[:phone_number].strip
-  email = params[:email_address].strip
+  first_name = params[:first_name].strip
+  last_name = params[:last_name].strip
+  phone_number = params[:phone_number].strip
+  email_address = params[:email_address].strip
   group = params[:group_name] == '' ? nil : params[:group_name]
 
-  contact = { first_name: fname, last_name: lname,
-              phone_number: phone, email_address: email,
-              group_name: group }
-  error = valid_contact?(contact)
+  # contact = { first_name: fname, last_name: lname,
+  #             phone_number: phone, email_address: email,
+  #             group_name: group }
+  error = valid_contact?(first_name, last_name, phone_number, email_address)
 
   if error
     session[:message] = error
     erb :new_contact
   else
-    session[:contacts] << contact
+    # session[:contacts] << contact
+    @storage.create_new_contact(first_name, last_name, phone_number, email_address, group)
     session[:message] = 'The contact has been created successfully.'
     redirect '/contacts'
   end
@@ -82,7 +88,8 @@ end
 # delete contact from contact list
 post '/contacts/:contact_id/destroy' do
   contact_id = params[:contact_id].to_i
-  session[:contacts].delete_at(contact_id)
+  # session[:contacts].delete_at(contact_id)
+  @storage.delete_contact_at_id(contact_id)
   session[:message] = 'The contact has been deleted.'
 
   redirect '/contacts'
@@ -91,7 +98,8 @@ end
 # render edit contact page
 get '/contacts/:contact_id/edit' do
   @contact_id = params[:contact_id].to_i
-  @contact = session[:contacts][@contact_id]
+  # @contact = session[:contacts][@contact_id]
+  @contact = @storage.find_contact(@contact_id)
 
   erb :edit_contact
 end
@@ -99,23 +107,23 @@ end
 # update contact info
 post '/contacts/:contact_id' do
   @contact_id = params[:contact_id].to_i
-  @contact = session[:contacts][@contact_id]
-  fname = params[:first_name].strip
-  lname = params[:last_name].strip
-  phone = params[:phone_number].strip
-  email = params[:email_address].strip
+  # @contact = session[:contacts][@contact_id]
+  @contact = @storage.find_contact(@contact_id)
+
+  first_name = params[:first_name].strip
+  last_name = params[:last_name].strip
+  phone_number = params[:phone_number].strip
+  email_address = params[:email_address].strip
   group = params[:group_name] == '' ? nil : params[:group_name]
 
-  updated_contact = { first_name: fname, last_name: lname,
-                      phone_number: phone, email_address: email,
-                      group_name: group }
-  error = valid_contact?(updated_contact)
+  error = valid_contact?(first_name, last_name, phone_number, email_address)
 
   if error
     session[:message] = error
     erb :edit_contact
   else
-    session[:contacts][@contact_id] = updated_contact
+    # session[:contacts][@contact_id] = updated_contact
+    @storage.update_contact(@contact_id, first_name, last_name, phone_number, email_address, group)
     session[:message] = 'The contact has been updated successfully.'
     redirect '/contacts'
   end
@@ -123,10 +131,11 @@ end
 
 # render contacts group page
 get '/contacts/groups' do
-  contacts_with_groups = session[:contacts].select { |contact| contact[:group_name] }
-  @group_names = contacts_with_groups.each_with_object([]) do |contact, arr|
-    arr << contact[:group_name] unless arr.include?(contact[:group_name])
-  end
+  # contacts_with_groups = session[:contacts].select { |contact| contact[:group_name] }
+  # @group_names = contacts_with_groups.each_with_object([]) do |contact, arr|
+  #   arr << contact[:group_name] unless arr.include?(contact[:group_name])
+  # end
+  @group_names = @storage.all_group_names
 
   erb :contact_groups
 end
